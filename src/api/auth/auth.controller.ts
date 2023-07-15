f53688c57988b871a3fd14f9105b1cff86f61d9e
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-07-15 13:11:35 +0100

    added API summaries

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { Public } from 'src/decorators/auth';
 import { APIResponse } from 'src/types/APIResponse';
 import { AuthService } from './auth.service';
@@ -12,6 +12,9 @@ import { RegisterDto } from './dtos/RegisterDto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Register new user',
+  })
   @Post('register')
   @Public()
   async register(
@@ -25,6 +28,9 @@ export class AuthController {
     } as AuthenticatedAccountDto);
   }
 
+  @ApiOperation({
+    summary: 'Login user',
+  })
   @Post('login')
   @HttpCode(200)
   @Public()
